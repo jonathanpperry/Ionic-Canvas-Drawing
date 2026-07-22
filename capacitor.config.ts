646280aadcd7e749536c14068f535705faec63e3
Jon Perry <jonperry113@gmail.com>
2026-07-22 12:32:21 -0400

    Fix app package name in capacitor config to match android java path

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.jonnyramen.painterapp',
-  appName: 'painter-app',
+  appId: 'io.jonnyramen.canvasdrawing',
+  appName: 'Canvas Drawing',
   webDir: 'www'
 };
 
